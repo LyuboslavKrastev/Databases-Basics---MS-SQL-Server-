a3e441f6c6c8ddb0ecb90b4708a46c33a0764a36
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2018-09-17 20:51:05 +0300

    Update 05. DB-Basics-MSSQL-Data-Aggregation-Exercises.sql

--- 05. DB-Basics-MSSQL-Data-Aggregation-Exercises/05. DB-Basics-MSSQL-Data-Aggregation-Exercises.sql
+++ 05. DB-Basics-MSSQL-Data-Aggregation-Exercises/05. DB-Basics-MSSQL-Data-Aggregation-Exercises.sql
@@ -71,6 +71,24 @@
 	WHEN Age Between 51 AND 60 THEN '[51-60]'
 	WHEN AGE >= 61 THEN '[61+]'
 	END 
+	
+	-- other variant, using a subquerry
+	
+	SELECT ageGroups.AgeGroup, COUNT(*) FROM
+	(
+	SELECT
+	CASE 
+	WHEN Age Between 0 AND 10 THEN '[0-10]'
+	WHEN Age Between 11 AND 20 THEN '[11-20]'
+	WHEN Age Between 21 AND 30 THEN '[21-30]'
+	WHEN Age Between 31 AND 40 THEN '[31-40]'
+	WHEN Age Between 41 AND 50 THEN '[41-50]'
+	WHEN Age Between 51 AND 60 THEN '[51-60]'
+	WHEN AGE >= 61 THEN '[61+]'
+	END AS AgeGroup
+	FROM WizzardDeposits
+	) AS ageGroups
+	GROUP BY ageGroups.AgeGroup
 
 --10
 
